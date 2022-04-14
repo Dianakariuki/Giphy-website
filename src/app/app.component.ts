8c08b0aa2db36523c87a10da0836a8cf840d96d3
Diana Kariuki <dianakariuki842@gmail.com>
2022-04-14 03:30:41 +0300

    This commit imports the components content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to GiphySearch';
-  performSearch(): void {
-    console.log('button click is working');
-}
+  title = 'GiphySearch';
 }
